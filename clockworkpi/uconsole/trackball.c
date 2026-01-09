473d99c673a490a0f8d2397169e400a36dd10b14
tweiss283 <mail@tweiss.cloud>
2026-01-09 14:07:46 +0100

    Match pointing_device_driver_init signature (return bool) to fix build

--- clockworkpi/uconsole/trackball.c
+++ clockworkpi/uconsole/trackball.c
@@ -73,7 +73,7 @@ static void trackball_right(void* arg) { (void)arg; trackball_move(AXIS_X, TB_IN
 static void trackball_up(void* arg) { (void)arg; trackball_move(AXIS_Y, TB_DECR); }
 static void trackball_down(void* arg) { (void)arg; trackball_move(AXIS_Y, TB_INCR); }
 
-void pointing_device_driver_init(void) {
+bool pointing_device_driver_init(void) {
     palSetLineMode(TB_LEFT, PAL_MODE_INPUT_PULLUP);
     palSetLineMode(TB_RIGHT, PAL_MODE_INPUT_PULLUP);
     palSetLineMode(TB_UP, PAL_MODE_INPUT_PULLUP);
@@ -88,6 +88,7 @@ void pointing_device_driver_init(void) {
     palSetLineCallback(TB_RIGHT, trackball_right, NULL);
     palSetLineCallback(TB_UP, trackball_up, NULL);
     palSetLineCallback(TB_DOWN, trackball_down, NULL);
+    return true;
 }
 
 report_mouse_t pointing_device_driver_get_report(report_mouse_t mouse_report) {
